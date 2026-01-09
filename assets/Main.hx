0d2712998eac9434b5d16f382fb09a1a45b105c3
alana2017para-spec <alana2017para@gmail.com>
2026-01-09 12:55:39 -0300

    Add files via upload

---
+++ assets/Main.hx
@@ -0,0 +1,12 @@
+
+package;
+
+import openfl.display.Sprite;
+import flixel.FlxGame;
+
+class Main extends Sprite {
+    public function new() {
+        super();
+        addChild(new FlxGame(0, 0, PlayState));
+    }
+}
