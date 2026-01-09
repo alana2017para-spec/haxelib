68cc2dac01dd8719c0c5d7798dc88f5bb68ab023
alana2017para-spec <alana2017para@gmail.com>
2026-01-09 12:30:56 -0300

    Add Main class with FlxGame initialization

--- assets/Main.hx
+++ assets/Main.hx
@@ -1,1 +1,13 @@
 
+package;
+
+import openfl.display.Sprite;
+import flixel.FlxGame;
+
+class Main extends Sprite {
+    public function new() {
+        super();
+        addChild(new FlxGame(0, 0, PlayState));
+    
+    }
+}
